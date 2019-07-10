6c1dc8c8f2f0c15b8e6918c451c14fe6bbf1cbfb
David Moten <davidmoten@gmail.com>
2019-07-10 14:48:54 +1000

    limit benchmarks to search

--- src/test/java/com/github/davidmoten/rtree/BenchmarksRTree.java
+++ src/test/java/com/github/davidmoten/rtree/BenchmarksRTree.java
@@ -66,197 +66,197 @@ public class BenchmarksRTree {
     private final RTree<Object, Rectangle> smallStarTreeM128 = RTree.maxChildren(128).star().<Object, Rectangle>create()
             .add(some);
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Point> defaultRTreeInsertOneEntryIntoGreekDataEntriesMaxChildren004() {
         return insertPoint(defaultTreeM4);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Point> defaultRTreeCreation010() {
         return RTree.maxChildren(10).<Object, Point>create().add(entries);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Point> starRTreeCreation010() {
         return RTree.maxChildren(10).star().<Object, Point>create().add(entries);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Point> bulkLoadingRTreeCreation010() {
         return RTree.maxChildren(10).<Object, Point>create(entries);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Point> bulkLoadingFullRTreeCreation010() {
         return RTree.maxChildren(10).loadingFactor(1.0).<Object, Point>create(entries);
     }
 
-    @Benchmark
+    // @Benchmark
     public long defaultRTreeSearchOfGreekDataPointsMaxChildren004() {
         return searchGreek(defaultTreeM4);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Point> defaultRTreeInsertOneEntryIntoGreekDataEntriesMaxChildren010() {
         return insertPoint(defaultTreeM10);
     }
 
-    @Benchmark
+    // @Benchmark
     public long defaultRTreeSearchOfGreekDataPointsMaxChildren010() {
         return searchGreek(defaultTreeM10);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Point> rStarTreeInsertOneEntryIntoGreekDataEntriesMaxChildren004() {
         return insertPoint(starTreeM4);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Point> rStarTreeInsertOneEntryIntoGreekDataEntriesMaxChildren010() {
         return insertPoint(starTreeM10);
     }
 
-    @Benchmark
+    // @Benchmark
     public long rStarTreeSearchOfGreekDataPointsMaxChildren004() {
         return searchGreek(starTreeM4);
     }
 
-    @Benchmark
+    // @Benchmark
     public long rStarTreeSearchOfGreekDataPointsMaxChildren010() {
         return searchGreek(starTreeM10);
     }
 
-    @Benchmark
+    // @Benchmark
     public void rStarTreeSearchOfGreekDataPointsMaxChildren010WithBackpressure(Blackhole bh) {
         searchGreekWithBackpressure(starTreeM10, bh);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Point> defaultRTreeInsertOneEntryIntoGreekDataEntriesMaxChildren032() {
         return insertPoint(defaultTreeM32);
     }
 
-    @Benchmark
+    // @Benchmark
     public long defaultRTreeSearchOfGreekDataPointsMaxChildren032() {
         return searchGreek(defaultTreeM32);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Point> rStarTreeInsertOneEntryIntoGreekDataEntriesMaxChildren032() {
         return insertPoint(starTreeM32);
     }
 
-    @Benchmark
+    // @Benchmark
     public long rStarTreeSearchOfGreekDataPointsMaxChildren032() {
         return searchGreek(starTreeM32);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Point> defaultRTreeInsertOneEntryIntoGreekDataEntriesMaxChildren128() {
         return insertPoint(defaultTreeM128);
     }
 
-    @Benchmark
+    // @Benchmark
     public long defaultRTreeSearchOfGreekDataPointsMaxChildren128() {
         return searchGreek(defaultTreeM128);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Point> rStarTreeInsertOneEntryIntoGreekDataEntriesMaxChildren128() {
         return insertPoint(starTreeM128);
     }
 
-    @Benchmark
+     @Benchmark
     public long rStarTreeSearchOfGreekDataPointsMaxChildren128() {
         return searchGreek(starTreeM128);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Rectangle> defaultRTreeInsertOneEntryInto1000EntriesMaxChildren004() {
         return insertRectangle(smallDefaultTreeM4);
     }
 
-    @Benchmark
+    // @Benchmark
     public long defaultRTreeSearchOf1000PointsMaxChildren004() {
         return search(smallDefaultTreeM4);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Rectangle> defaultRTreeInsertOneEntryInto1000EntriesMaxChildren010() {
         return insertRectangle(smallDefaultTreeM10);
     }
 
-    @Benchmark
+    // @Benchmark
     public long defaultRTreeSearchOf1000PointsMaxChildren010() {
         return search(smallDefaultTreeM10);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Rectangle> rStarTreeInsertOneEntryInto1000EntriesMaxChildren004() {
         return insertRectangle(smallStarTreeM4);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Rectangle> rStarTreeInsertOneEntryInto1000EntriesMaxChildren010() {
         return insertRectangle(smallStarTreeM10);
     }
 
-    @Benchmark
+     @Benchmark
     public void rStarTreeSearchOf1000PointsMaxChildren004() {
         search(smallStarTreeM4);
     }
 
-    @Benchmark
+     @Benchmark
     public long rStarTreeSearchOf1000PointsMaxChildren010() {
         return search(smallStarTreeM10);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Rectangle> defaultRTreeInsertOneEntryInto1000EntriesMaxChildren032() {
         return insertRectangle(smallDefaultTreeM32);
     }
 
-    @Benchmark
+    // @Benchmark
     public long defaultRTreeSearchOf1000PointsMaxChildren032() {
         return search(smallDefaultTreeM32);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Rectangle> rStarTreeInsertOneEntryInto1000EntriesMaxChildren032() {
         return insertRectangle(smallStarTreeM32);
     }
 
-    @Benchmark
+     @Benchmark
     public long rStarTreeSearchOf1000PointsMaxChildren032() {
         return search(smallStarTreeM32);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Rectangle> defaultRTreeInsertOneEntryInto1000EntriesMaxChildren128() {
         return insertRectangle(smallDefaultTreeM128);
     }
 
-    @Benchmark
+    // @Benchmark
     public long defaultRTreeSearchOf1000PointsMaxChildren128() {
         return search(smallDefaultTreeM128);
     }
 
-    @Benchmark
+    // @Benchmark
     public RTree<Object, Rectangle> rStarTreeInsertOneEntryInto1000EntriesMaxChildren128() {
         return insertRectangle(smallStarTreeM128);
     }
 
-    @Benchmark
+     @Benchmark
     public long rStarTreeSearchOf1000PointsMaxChildren128() {
         return search(smallStarTreeM128);
     }
 
-    @Benchmark
+    // @Benchmark
     public void rStarTreeDeleteOneEveryOccurrenceFromGreekDataChildren010() {
         deleteAll(starTreeM10);
     }
 
-    @Benchmark
+     @Benchmark
     public long searchNearestGreek() {
         return searchNearestGreek(starTreeM4);
     }
