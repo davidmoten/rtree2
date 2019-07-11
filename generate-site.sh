86587d5f4943004c5e88c57ff0129f1656c5c2fa
Dave Moten <davidmoten@gmail.com>
2019-07-11 10:21:21 +1000

    update generate-site.sh

--- generate-site.sh
+++ generate-site.sh
@@ -3,8 +3,8 @@ set -e
 mvn site
 cd ../davidmoten.github.io
 git pull
-mkdir -p rtree
-cp -r ../rtree/target/site/* rtree/
+mkdir -p rtree2
+cp -r ../rtree2/target/site/* rtree2/
 git add .
 git commit -am "update site reports"
 git push
