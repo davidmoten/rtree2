9d37b7d6d1629d1415003aa6ef3891fe94c828cc
up-to-you <sabirovdev@gmail.com>
2020-04-06 20:21:18 +0300

    actual module-info + java 11 target

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.github.davidmoten.rtree2 {
+    requires java.desktop;
+
+    exports com.github.davidmoten.guavamini;
+    exports com.github.davidmoten.rtree2;
+    exports com.github.davidmoten.rtree2.geometry;
+}
