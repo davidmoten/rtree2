126c05e2bad1df5da9a7daa0efc37518a858128d
Viktor Polak <viktor.polak@tracmap.com>
2023-03-24 10:16:56 +1300

    Polygon docstrings, comments, exceptions, renaming

--- src/main/java/com/github/davidmoten/rtree2/geometry/internal/PolygonDouble.java
+++ src/main/java/com/github/davidmoten/rtree2/geometry/internal/PolygonDouble.java
@@ -5,6 +5,9 @@ import com.github.davidmoten.rtree2.internal.util.ObjectsHelper;
 
 import java.util.ArrayList;
 
+/**
+ * A convex polygon shell.
+ */
 public final class PolygonDouble implements Polygon {
 
     private final ArrayList<PointDouble> points;
@@ -32,6 +35,12 @@ public final class PolygonDouble implements Polygon {
         mbr = RectangleDouble.create(minX, minY, maxX, maxY);
     }
 
+    /**
+     * Constructor for a convex polygon shell.
+     * @param coordinates An array of x1, y1, x2, y2, ... xn, yn that make the un-closed shell of a convex polygon.
+     *                    As it is a polygon, at least 3 pairs of points are required (6 values).
+     * @return convex polygon shell
+     */
     public static PolygonDouble create(double[] coordinates) {
         return new PolygonDouble(coordinates);
     }
@@ -43,17 +52,17 @@ public final class PolygonDouble implements Polygon {
 
     @Override
     public double distance(Rectangle r) {
-        throw new RuntimeException("not implemented");
+        throw new UnsupportedOperationException("not implemented");
     }
 
     @Override
     public boolean intersects(Rectangle r) {
-        throw new RuntimeException("not implemented");
+        throw new UnsupportedOperationException("not implemented");
     }
 
     @Override
     public boolean intersects(Circle c) {
-        throw new RuntimeException("not implemented");
+        throw new UnsupportedOperationException("not implemented");
     }
 
     @Override
@@ -73,17 +82,15 @@ public final class PolygonDouble implements Polygon {
     @Override
     public boolean intersects(Point point) {
         int n = points.size();
-        if (n < 3)
-            return false;
 
-        int firstSign = cosineSign(points.get(n - 1), point, points.get(0));
-        if (firstSign == 0)
-            return false;
+        int firstDirection = pointsDirection(points.get(n - 1), point, points.get(0));
+        if (firstDirection == 0)
+            return true;
 
         for (int i = 0; i < n - 1; i++) {
-            int sign = cosineSign(points.get(i), point, points.get(i + 1));
-            if (sign == 0 || sign != firstSign)
-                return false;
+            int sign = pointsDirection(points.get(i), point, points.get(i + 1));
+            if (sign == 0) return true;
+            if (sign != firstDirection) return false;
         }
 
         return true;
@@ -91,8 +98,10 @@ public final class PolygonDouble implements Polygon {
 
     @Override
     public boolean intersects(Line line) {
+        // handle the case when the line is fully inside the polygon
         if (intersects(PointDouble.create(line.x1(), line.y1())))
             return true;
+        // check if the line intersects any of the edges of this polygon
         int n = points.size();
         for (int i = 0; i < n; i++) {
             PointDouble cur = points.get(i);
@@ -109,9 +118,16 @@ public final class PolygonDouble implements Polygon {
         return true;
     }
 
-    private static int cosineSign(Point a, Point b, Point c) {
-        double cosine = (b.x() - a.x()) * (c.y() - a.y()) - (b.y() - a.y()) * (c.x() - a.x());
-        if (Math.abs(cosine) < PRECISION) return 0;
-        return cosine > 0.0 ? 1 : -1;
+    /**
+     * Return the orientation of point b relative to the path a -> c.
+     * @param a origin point
+     * @param b point to determine if it is to the left, right or along the path
+     * @param c point we are facing from a
+     * @return 0 if b is on the line a->c, -1 if b is to the left of a facing c or 1 if to the right.
+     */
+    private static int pointsDirection(Point a, Point b, Point c) {
+        double crossProduct = (b.x() - a.x()) * (c.y() - a.y()) - (b.y() - a.y()) * (c.x() - a.x());
+        if (Math.abs(crossProduct) < PRECISION) return 0;
+        return crossProduct > 0.0 ? 1 : -1;
     }
 }
